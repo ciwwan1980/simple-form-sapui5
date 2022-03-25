4999b4e4bd21e5475e408136366d09113dd29dc4
ABDALQADER Ajil <ajil.abdalqader@soprasteria.com>
2022-03-25 13:30:12 +0100

    setting-up-view and controller

---
+++ webapp/controller/Login.controller.js
@@ -0,0 +1,14 @@
+sap.ui.define([
+   "sap/ui/core/mvc/Controller",
+], function (Controller) {
+   "use strict";
+   return Controller.extend("tom.controller.Login", {
+     
+      onInit: function () {
+      
+      },
+      onLogin: function () {
+      var oInpUser= this.getView("idUser")
+      },
+   });
+});
