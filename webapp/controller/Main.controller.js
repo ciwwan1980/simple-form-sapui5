5ea9af1233dc6d98524691af6b78e41ffc57cc56
ABDALQADER Ajil <ajil.abdalqader@soprasteria.com>
2022-03-29 11:13:15 +0200

    another-way for data-binding

--- webapp/controller/Main.controller.js
+++ webapp/controller/Main.controller.js
@@ -1,35 +1,49 @@
 sap.ui.define([
     "ibm/fin/ar/controller/BaseController",
- ], function (Controller) {
- 
+], function (Controller) {
+
     return Controller.extend("ibm.fin.ar.controller.Main", {
- 
-      
-          onInit: function () {
+
+
+        onInit: function () {
 
 
             ///steps 1: create a brand new model object 
-                var oSpiderman= new sap.ui.model.json.JSONModel();
+            var oSpiderman = new sap.ui.model.json.JSONModel();
             // steps 2- set or load date in the model
-                oSpiderman.setData({ 
-                    "empStr":{
-                        "empId":100,
-                        "empName":"ajil",
-                        "salary":9500,
-                        "currency":"euro",
-                        "sherlok": false
-                    }
-                
-                })
-                //steps 3- make a model awar to the application
-                sap.ui.getCore().setModel(oSpiderman)
-                
-                //steps4: binding - 4 ways we can do binding-syntac
-                var oSalary=this.getView().byId("idSalary");
-                oSalary.bindValue("/empStr/salary");
-                var oCurr=this.getView().byId("idCurr");
-                oCurr.bindProperty("value", "/empStr/currency")
-                oCurr.bindProperty("enabled", "/empStr/sherlok")
+            oSpiderman.setData({
+                "empStr": {
+                    "empId": 100,
+                    "empName": "ajil",
+                    "salary": 9500,
+                    "currency": "euro",
+                    "sherlok": false
+                }
+
+            })
+            //steps 3- make a model awar to the application
+            sap.ui.getCore().setModel(oSpiderman)
+
+            // //steps4: binding - 4 ways we can do binding-syntac
+            // var oSalary = this.getView().byId("idSalary");
+            // oSalary.bindValue("/empStr/salary");
+            // var oCurr = this.getView().byId("idCurr");
+            // oCurr.bindProperty("value", "/empStr/currency")
+            // oCurr.bindProperty("enabled", "/empStr/sherlok")
+
+        },
+        // complex-depening on ID in View
+        onMagic: function () {
+            var aForm = this.getView().byId("simpleForm");
+            var aContent = aForm.getContent();
+            for (let i = 0; i <aContent.length; i++) {
+                const element = aContent[i];
+              if(element.getMetadata().getName()==="sap.m.input"){
+                    element.setEnabled(true)
+              }  
+
             }
-       });
- })
+        }
+
+    });
+})
