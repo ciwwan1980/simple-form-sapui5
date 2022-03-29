2bcb79242dc57d0497fc412ece4752ce56bcc76e
ABDALQADER Ajil <ajil.abdalqader@soprasteria.com>
2022-03-29 11:28:08 +0200

    dynamicly change the value with setPropert

--- webapp/controller/Main.controller.js
+++ webapp/controller/Main.controller.js
@@ -24,25 +24,35 @@ sap.ui.define([
             //steps 3- make a model awar to the application
             sap.ui.getCore().setModel(oSpiderman)
 
-            // //steps4: binding - 4 ways we can do binding-syntac
-            // var oSalary = this.getView().byId("idSalary");
-            // oSalary.bindValue("/empStr/salary");
-            // var oCurr = this.getView().byId("idCurr");
-            // oCurr.bindProperty("value", "/empStr/currency")
+            //steps4: binding - 4 ways we can do binding-syntac
+            var oSalary = this.getView().byId("idSalary");
+            oSalary.bindValue("/empStr/salary");
+            var oCurr = this.getView().byId("idCurr");
+            oCurr.bindProperty("value", "/empStr/currency")
             // oCurr.bindProperty("enabled", "/empStr/sherlok")
 
         },
         // complex-depening on ID in View
         onMagic: function () {
-            var aForm = this.getView().byId("simpleForm");
-            var aContent = aForm.getContent();
-            for (let i = 0; i <aContent.length; i++) {
-                const element = aContent[i];
-              if(element.getMetadata().getName()==="sap.m.input"){
-                    element.setEnabled(true)
-              }  
+            
+        //technique number 2- 
+            // var aForm = this.getView().byId("simpleForm");
+            // var aContent = aForm.getContent();
+            // for (let i = 0; i <aContent.length; i++) {
+            //     const element = aContent[i];
+            //   if(element.getMetadata().getName()==="sap.m.input"){
+            //         element.setEnabled(true)
+            //   }  
 
-            }
+            // }
+
+        //technique number 3- 
+            
+            //1- get the model of the object again, after we set the model in onint 
+            // sap.ui.getCore().setModel(oSpiderman), we get it again below
+            var oModel= sap.ui.getCore().getModel();
+            //2-change the value in the model for scherlock 
+            oModel.setProperty("/empStr/sherlok", true)
         }
 
     });
