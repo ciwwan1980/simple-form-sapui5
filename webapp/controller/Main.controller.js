f3f533067e29dbdd46131c50934518bbc11d8dc4
ABDALQADER Ajil <ajil.abdalqader@soprasteria.com>
2022-04-16 22:50:14 +0200

    adding table

--- webapp/controller/Main.controller.js
+++ webapp/controller/Main.controller.js
@@ -7,14 +7,13 @@ sap.ui.define([
 
         onInit: function () {
 
-        //   var oSpiderman= model.createMyJSONModel()
+            //var oSpiderman= model.createMyJSONModel()
             ///steps 1: create a brand new model object 
             // steps 2- set or load date in the model
             //steps 3- make a model awar to the application
             var oSpiderman = model.createMyJSONModel("models/mockdata/sample.json"); 
             var oIronman = model.createMyJSONModel("models/mockdata/sample1.json");
-
-
+            
             // i shall save them in variable in case if i have 2 models
             // the first oen is default one 
             sap.ui.getCore().setModel(oSpiderman)
@@ -27,8 +26,8 @@ sap.ui.define([
              this._bindValue()
           
         },
-        // complex-depening on ID in View
 
+     // complex-depening on ID in View
             _bindValue: function(){
                //steps4: binding - 4 ways we can do binding-syntac
             var oSalary = this.getView().byId("idSalary");
@@ -38,6 +37,7 @@ sap.ui.define([
             // oCurr.bindProperty("enabled", "/empStr/sherlok")
            
             },
+
             oCore:sap.ui.getCore(),
             onChange:function(){
                 var oSpiderman=this.oCore.getModel();
@@ -55,9 +55,8 @@ sap.ui.define([
             // for (let i = 0; i <aContent.length; i++) {
             //     const element = aContent[i];
             //   if(element.getMetadata().getName()==="sap.m.input"){
-            //         element.setEnabled(true)
+            //      element.setEnabled(true)
             //   }  
-
             // }
 
             if(this.mario===true){
@@ -67,7 +66,6 @@ sap.ui.define([
             }
 
         //technique number 3- 
-            
             //1- get the model of the object again, after we set the model in onint 
             // sap.ui.getCore().setModel(oSpiderman), we get it again below
             var oModel= this.getView().getModel();
